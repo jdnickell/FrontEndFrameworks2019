fc5a9db477d02024d353895a5bfd10afcf8630d3
jd nickell <jd@jdnickell.com>
2019-01-20 12:48:22 -0600

    add router and links

--- react-todo/src/components/layout/Header.js
+++ react-todo/src/components/layout/Header.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 //functional component acts as only returning the 'render' method of a class lifecycle
 export default function Header() {
   return (
     <header className='todo-header'>
         <h1>TodoList</h1>
+        <Link to='/'>Home</Link> |
+        <Link to='/about'>About</Link>
     </header>
   )
 }
