000620a78ba56d72d35275396630b548a7c22096
jd nickell <jd@jdnickell.com>
2019-01-27 08:25:49 -0600

    add router, axios and jsonplaceholder functionality

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,47 +1,20 @@
 <template>
   <div id="app">
-    <Header />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <Header />    
+    <router-view/><!-- current view embedded here -->
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
-
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Todos
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: 'todo one',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'todo two',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'todo three',
-          completed: false
-        }                
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id != id);
-    }
+    Header
   }
 }
 </script>
+
 
 <style>
   *{
@@ -54,4 +27,18 @@ export default {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
   }
+
+  .btn{
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover{
+    background: #666;
+  }
+
 </style>
--- vue-todo/src/components/layout/Header.vue
+++ vue-todo/src/components/layout/Header.vue
@@ -1,6 +1,10 @@
 <template>
     <header>
-        <h1>TodoList</h1>
+        <h1>Todo List</h1>
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
     </header>
 </template>
 
@@ -11,15 +15,16 @@ export default {
 </script>
 
 <style scoped>
-    .header{
+    header {
         background: #333;
         color: #fff;
         text-align: center;
         padding: 10px;
     }
 
-    .header a{
+    header a {
         color: #fff;
         padding-right: 5px;
+        text-decoration: none;
     }
 </style>
